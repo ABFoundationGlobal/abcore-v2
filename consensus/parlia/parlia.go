50074e7c60f4f0b345b12e9c060ed073c2ffad6e
chendehai <249934074@qq.com>
2026-04-03 15:14:37 +0800

    Revert "fix(parlia): allow prepareValidators to run at non-epoch ParliaGenesisBlock"
    This reverts commit 69336aeddc9779d12035ec8fbe83b8c817cf0459.

--- consensus/parlia/parlia.go
+++ consensus/parlia/parlia.go
@@ -1090,10 +1090,7 @@ func (p *Parlia) prepareValidators(chain consensus.ChainHeaderReader, header *ty
 	if err != nil {
 		return err
 	}
-	// IsOnParliaGenesis must be checked before the epoch boundary guard: ParliaGenesisBlock
-	// is not guaranteed to fall on an epoch boundary, but always needs validators encoded so
-	// that applyParliaGenesisUpgrade can deploy the ValidatorSet contract with the correct set.
-	if header.Number.Uint64()%epochLength != 0 && !p.chainConfig.IsOnParliaGenesis(header.Number) {
+	if header.Number.Uint64()%epochLength != 0 {
 		return nil
 	}
 
